5cdf48134796317d5f6c7c4b75b1762426d64d84
Cau� Olive Barbosa <52267575817@>
2025-04-04 15:17:43 -0300

    Five Test

--- clinica/urls.py
+++ clinica/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('/medicos/', views.listar_medicos),
     path('/consultas/nova/', views.criar_consulta),
-    path('/consultas/<int:id>/', views.detalhes_consulta)
+    path('/consultas/<int:id>/', views.detalhes_consulta),
 ]
