a129e2ee055e22c77308e3948586ce31ad0267db
Cau� Olive Barbosa <52267575817@>
2025-04-04 16:32:28 -0300

    Teste 13

--- clinica/views.py
+++ clinica/views.py
@@ -5,24 +5,22 @@ from rest_framework.decorators import api_view
 from .models import Medico,Consulta
 
 
-
-
 def listar_medicos(request):
     if request.method == 'GET':
         medicos = Medico.objects.all()
-        return medicos
+        return Response(medicos,status=status.HTTP_200_OK)
 
 def criar_consulta(request):
     if request.method == 'POST':
         pacientes = request.POST.get('paciente')
         data = request.POST.get('data')
         medico = request.POST.get('medico')
-        status = request.POST.get('status')
-        Consulta.objects.create(pacientes = pacientes, data=data, medico=medico, status=status)
-        return Response( status=status.HTTP_201_CREATED)
+        estado = request.POST.get('status')
+        Consulta.objects.create(pacientes=pacientes, data=data, medico=medico, status=estado)
+    return Response( status=status.HTTP_201_CREATED)
 
 
 def detalhes_consulta(request):
     if request.method == 'GET':
         consulta_info = Consulta.objects.all()
-        return consulta_info
+        return Response(consulta_info, status=status.HTTP_200_OK)
