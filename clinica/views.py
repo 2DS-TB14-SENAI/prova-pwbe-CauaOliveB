821e93680b855131df90033f8d73c043192fc9d0
Cau� Olive Barbosa <52267575817@>
2025-04-04 16:36:05 -0300

    Teste 14

--- clinica/views.py
+++ clinica/views.py
@@ -9,6 +9,8 @@ def listar_medicos(request):
     if request.method == 'GET':
         medicos = Medico.objects.all()
         return Response(medicos,status=status.HTTP_200_OK)
+    return render (request,'listar_medicos.html')
+
 
 def criar_consulta(request):
     if request.method == 'POST':
@@ -17,10 +19,12 @@ def criar_consulta(request):
         medico = request.POST.get('medico')
         estado = request.POST.get('status')
         Consulta.objects.create(pacientes=pacientes, data=data, medico=medico, status=estado)
-    return Response( status=status.HTTP_201_CREATED)
+        return Response( status=status.HTTP_201_CREATED)
+    return render (request,'form_consulta.html')
 
 
 def detalhes_consulta(request):
     if request.method == 'GET':
         consulta_info = Consulta.objects.all()
         return Response(consulta_info, status=status.HTTP_200_OK)
+    return render(request, '')
